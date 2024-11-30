962d15fae03424595e2f37f0d2692fc3bd3637f8
Snowrk <shinsan555555@gmail.com>
2024-11-30 23:25:13 +0530

    in progress

--- app/page.js
+++ app/page.js
@@ -1,95 +1,85 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
+import "./page.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const list = [
+    {
+      id: 1,
+      name: "Apple",
+      imgUrl: "https://assets.codepen.io/128542/apple.png",
+    },
+    {
+      id: 2,
+      name: "Grape",
+      imgUrl: "https://assets.codepen.io/128542/grape.png",
+    },
+    {
+      id: 3,
+      name: "Orange",
+      imgUrl: "https://assets.codepen.io/128542/orange.png",
+    },
+    {
+      id: 4,
+      name: "Bananna",
+      imgUrl: "https://assets.codepen.io/128542/banana.png",
+    },
+    {
+      id: 5,
+      name: "Cherry",
+      imgUrl: "https://assets.codepen.io/128542/cherry.png",
+    },
+  ];
+  const handlePointerDown = (e) => {
+    e.target.setPointerCapture(e.pointerId);
+    e.target.style.cursor = "grabbing";
+    // const ele = e.target.parentNode.cloneNode(true);
+    // ele.classList.add("draggingIcon");
+    // const listNode = e.target.parentNode.parentNode;
+    // listNode.appendChild(ele);
+    // document.body.classList.add("grabbing");
+    // const r = document.querySelector(":root");
+    // let mouseX = `${e.pageX}px`;
+    // let mouseY = `${e.pageY}px`;
+    // console.log(e);
+    // console.log(mouseX, mouseY);
+    // r.style.setProperty("--mouseX", mouseX);
+    // r.style.setProperty("--mouseY", mouseY);
+  };
+  const handlePointerUp = (e) => {
+    e.target.releasePointerCapture(e.pointerId);
+    e.target.style.cursor = "grab";
+  };
+  const ListItem = (props) => {
+    const { name, imgUrl } = props;
+    return (
+      <li className="listItem">
+        <span
+          className="dragIcon"
+          onPointerDown={handlePointerDown}
+          onPointerUp={handlePointerUp}
+        ></span>
+        <img src={imgUrl} alt={name} />
+        <p>{name}</p>
+        <div className="buttonWrapper">
+          <button>
+            <FaAngleUp />
+          </button>
+          <button>
+            <FaAngleDown />
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </li>
+    );
+  };
+  return (
+    <div className="con">
+      <ul className="list">
+        {list.map((item) => (
+          <ListItem name={item.name} imgUrl={item.imgUrl} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
